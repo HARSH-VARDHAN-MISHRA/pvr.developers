3e6c3e44f92e047a0e5e6a36978b6a2893cb26c0
HARSH-VARDHAN-MISHRA <mharshvardhan40@gmail.com>
2025-09-25 00:54:42 +0530

    update changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,28 @@ closeBtn.addEventListener("click", () => {
   navbar.classList.remove("show");
   hamburger.classList.remove("active");
 });
+
+
+
+
+
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  const galleryItems = document.querySelectorAll(".gallery-item");
+
+  filterBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      // Active button styling
+      filterBtns.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+
+      const filter = btn.getAttribute("data-filter");
+
+      galleryItems.forEach(item => {
+        if (filter === "all" || item.classList.contains(filter)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
